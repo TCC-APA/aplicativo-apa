36aaec6fca80ec3674ee92db8d7637b36c331046
Fernando Godoy <nanu.godoy@hotmail.com>
2019-08-27 21:06:07 -0300

    criacao activity questionario

--- PerfilDeAprendizado/app/src/main/java/com/ppa/perfildeaprendizado/PerguntasPessoaisActivity.java
+++ PerfilDeAprendizado/app/src/main/java/com/ppa/perfildeaprendizado/PerguntasPessoaisActivity.java
@@ -2,7 +2,9 @@ package com.ppa.perfildeaprendizado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PerguntasPessoaisActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class PerguntasPessoaisActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perguntas_pessoais);
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(PerguntasPessoaisActivity.this, QuestionarioActivity.class);
+        startActivity(intent);
+
     }
 }
--- PerfilDeAprendizado/app/src/main/java/com/ppa/perfildeaprendizado/ui/login/LoginViewModel.java
+++ PerfilDeAprendizado/app/src/main/java/com/ppa/perfildeaprendizado/ui/login/LoginViewModel.java
@@ -35,7 +35,7 @@ public class LoginViewModel extends ViewModel {
 
         if (result instanceof Result.Success) {
             Aluno data = ((Result.Success<Aluno>) result).getData();
-            loginResult.setValue(new LoginResult(new LoggedInUserView(data.getDisplayName())));
+            //loginResult.setValue(new LoginResult(new LoggedInUserView(data.getDisplayName())));
         } else {
             loginResult.setValue(new LoginResult(R.string.login_failed));
         }
