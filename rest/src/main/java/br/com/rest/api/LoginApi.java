b1466bc46b7ed2544b1edb85448bca66051b92d8
Fernando Godoy <nanu.godoy@hotmail.com>
2019-11-26 21:42:21 -0300

    alteracao de buscar aluno por matricula e senha para post

--- rest/src/main/java/br/com/rest/api/LoginApi.java
+++ rest/src/main/java/br/com/rest/api/LoginApi.java
@@ -1,30 +1,29 @@
 package br.com.rest.api;
 
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import br.com.rest.model.dto.LoginDTO;
 import br.com.rest.model.entity.AlunoEntity;
 import br.com.rest.services.AlunoServices;
 
 @Path("/login")
-public class LoginApi {	
-		
+public class LoginApi {
+
 	@POST
 	@Path("inserir")
-	@Produces("application/octet-stream")
+	@Produces(MediaType.APPLICATION_JSON)
 	public String inserirAluno(AlunoEntity aluno) {
 		return AlunoServices.incluirAluno(aluno).toString();
 	}
-	
-	@GET
+
+	@POST
 	@Path("")
 	@Produces(MediaType.APPLICATION_JSON)
-	public AlunoEntity realizarLoginPorMatriculaSenha(@QueryParam("matricula") String matricula, @QueryParam("senha") String senha) {
-		return AlunoServices.consultarAlunoPorMatriculaSenha(matricula, senha);
+	public AlunoEntity realizarLoginPorMatriculaSenha(LoginDTO login) {
+		return AlunoServices.consultarAlunoPorMatriculaSenha(login.getMatricula(), login.getSenha());
 	}
 
 }
